aae3add150a6b0d4e070f83ea33b207e48070d69
Daniel Souza <daniel.souza.af@gmail.com>
2018-03-07 17:44:22 -0300

    criptocurrency study

--- ghibli-app/src/App.js
+++ ghibli-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Tickers from './components/Tickers.js';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2>Studio Ghibli API Study</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Tickers />
       </div>
     );
   }
---
+++ ghibli-app/src/components/Tickers.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './Tickers.css';
+import Cryptocurrency from './Cryptocurrency'
+import axios from 'axios';
+
+class Tickers extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    id: "bitcoin",
+                    name: "Bitcoin",
+                    symbol: "BTC",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                },
+                {
+                    id: "ethereum",
+                    name: "Ethereum",
+                    symbol: "ETH",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                },
+                {
+                    id: "litecoin",
+                    name: "Litecoin",
+                    symbol: "LTC",
+                    price_usd: "1",
+                    percent_change_1h: "0",
+                    percent_change_24h: "0",
+                    percent_change_7d: "0",
+                }
+            ]
+        };
+    }
+
+    fetchCryptocurrencyData() {
+        axios.get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
+            .then(response => {
+                let wanted = ["bitcoin", "ethereum", "litecoin"];
+                let result = response.data.filter(currency => wanted.includes(currency.id));
+                this.setState({ data: result});
+            })
+            .catch(err => console.log(err));
+    }
+
+    componentDidMount() {
+        this.fetchCryptocurrencyData();
+        this.interval = setInterval(() => this.fetchCryptocurrencyData(), 10 * 1000);
+    }
+
+    render() {
+        let tickers = this.state.data.map((currency) =>
+            <Cryptocurrency data={currency} key={currency.id} />
+        );
+        return (
+            <div className="tickers-container">
+                <ul className="tickers">{tickers}</ul>
+                <p>Informação atualizada a cada 10 segundos</p>
+            </div>
+        );
+    }
+}
+
+export default Tickers;
